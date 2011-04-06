fd9574f566f911722dc710873bd7467c37e44547
Wiggins <code@hlidskialf.com>
2011-04-06 00:21:39 -0700

    dispatching irc client

--- jirc.py
+++ jirc.py
@@ -6,7 +6,9 @@ from pyxmpp import streamtls
 
 
 import asyncore
+import asynchat
 import socket
+import StringIO
 
 
 
@@ -19,49 +21,83 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 
+class AsyncIrcClient(asynchat.async_chat):
 
-class JircIrcClient(asyncore.dispatcher):
     def __init__(self, nick, user, host, port=6667, password=None):
-        asyncore.dispatcher.__init__(self)
+        asynchat.async_chat.__init__(self)
         self.nick = nick
         self.user = user
         self.host = host
         self.port = port
         self.password = password
+        self.buffer = StringIO.StringIO()
 
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect( (self.host, self.port) )
-        self.buffer = StringIO.StringIO()
+
+        self.set_terminator("\r\n")
 
         if password:
             self.output("PASS", self.password)
         self.output("NICK", self.nick)
         self.output("USER", self.user, '*', '*', ':'+self.user)
 
+    def collect_incoming_data(self, data):
+        self.buffer.write(data)
+
+    def found_terminator(self):
+        self.buffer.seek(0)
+        line = self.buffer.read()
+        self.buffer.truncate(0)
+        
+        prefix, command, args = self._parse_msg(line)
+
+        logger.info( "IRC: %s" % ((prefix,command,args),) )
+
+        handler = getattr(self, 'handle_%s' % (command.lower(),), None)
+        if handler is not None and callable(handler):
+            handler(prefix, command, args)
+
+
     def output(self, *args):
-        self.buffer.write(' '.join(args)+"\r\n")
+        self.push(' '.join(args)+"\r\n")
 
     def tick(self, timeout=1):
-        asyncore.loop(timeout=1, count=1)
+        asyncore.loop(timeout=timeout, count=1)
 
-    def handle_connect(self):
-        pass
-
-    def handle_close(self):
-        self.close()
-
-    def handle_read(self):
-        logger.info( "IRC: %s" % (self.recv(8192),) )
-
-    def handle_write(self):
-        sent = self.send(self.buffer)
-        self.buffer = self.buffer[sent:]
-
-    def writable(self):
-        return (len(self.buffer) > 0)
+    def loop(self, timeout=1):
+        asyncore.loop(timeout=timeout)
 
 
+    def _parse_msg(self,s):
+        prefix = ''
+        trailing = []
+        if not s:
+            return None
+        if s[0] == ':':
+            prefix, s = s[1:].split(' ', 1)
+        if s.find(' :') != -1:
+            s, trailing = s.split(' :', 1)
+            args = s.split()
+            args.append(trailing)
+        else:
+            args = s.split()
+        command = args.pop(0)
+        return prefix, command, args
 
+
+
+
+class JircIrcClient(AsyncIrcClient):
+
+    def join_channel(self, channel):
+        self.output("JOIN", channel)
+
+    def handle_376(self, prefix, command, args):
+        logger.info("END OF MOTD!!!!!")
+
+    def handle_privmsg(self, prefix, cmd, args):
+        logger.info("<%s> %s" % (prefix, args[1]))
 
 
 
@@ -169,12 +205,12 @@ class JircJabberClient(JabberClient):
 if __name__ == '__main__':
 
 
-
-    jjc = JircJabberClient("jirc@literat.us/bridge", password="Eel7Xeiz", server="literat.us");
+    jjc = JircJabberClient("jirc@literat.us/bridge", password="Eel7Xeiz", server="literat.us")
     jjc.connect()
 
 
-    jic = JircIrcClient("jirc", "jirc", "literat.us");
+    jic = JircIrcClient("jirc", "jirc", "literat.us")
+    jic.join_channel("#jirc")
 
 
     # main loop
